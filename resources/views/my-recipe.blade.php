2143dbc043410fe64bb5a36d81104de5796715b1
keniiiiiii <kenidumb112@gmail.com>
2024-12-15 22:05:00 +0800

    my recipes ui

--- resources/views/my-recipe.blade.php
+++ resources/views/my-recipe.blade.php
@@ -7,18 +7,19 @@
 @section('content')
     <!--MY RECIPE SECTION-->
     <div class="container">
-        <h1>My Recipes</h1>
-        <p class="recipes-count">0 Recipes</p>
         <div class="bookmarks-section">
             <div class="image">
                 <img src="/assets/images/recipe.png" alt="Recipe illustration">
             </div>
             <div class="text">
+                <h1>My Recipes</h1>
                 <h2>No recipes yet</h2>
                 <p>Want to create a recipe? Just click on the plus sign and select "Add Recipe".</p>
                 <a href="/menu2" class="button1">EXPLORE RECIPES</a>
             </div>
         </div>
+
+        <button class="add-btn">+</button>
     </div>
     <!--END OF MY RECIPE SECTION-->
 @endsection
